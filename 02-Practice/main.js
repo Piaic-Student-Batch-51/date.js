d3a089b39167b918b0742ba9b734109b8b8ad0de
Piaic-Student-Batch-51 <159433426+Piaic-Student-Batch-51@users.noreply.github.com>
2024-03-04 06:29:03 +0000

    Dates

---
+++ 02-Practice/main.js
@@ -0,0 +1,11 @@
+// Dates
+// let myDate = new Date();
+// console.log(myDate.toString());
+// console.log(myDate.toISOString());
+// console.log(myDate.toLocaleString());
+// console.log(myDate.toJSON());
+// console.log(typeof myDate);
+
+
+let myCreatedDate = new Date(2024, 0, 23);
+console.log(myCreatedDate.toDateString());
